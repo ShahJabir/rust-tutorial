2c21883ad1e100ba11e4900f995d8da193c3a621
ShahJabir <shahjabirofficial@gmail.com>
2026-01-10 23:18:05 +0600

    learn: iter filter method

--- 23_iterator_methods/src/main.rs
+++ 23_iterator_methods/src/main.rs
@@ -1,7 +1,15 @@
 fn main() {
+    // Map
     let vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let double_vec: Vec<i32> = vec.iter().map(|x|x*2).collect();
     println!("{:?}",double_vec);
     let double_vec: Vec<i32> = vec.into_iter().map(|x| x*2).collect();
     println!("{:?}", double_vec);
+
+    // Filter
+    let vec: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let even_vec:Vec<&i32> = vec.iter().filter(|x| *x%2 == 0).collect();
+    println!("{:?}",even_vec);
+    let even_vec:Vec<i32> = vec.into_iter().filter(|x| x%2 == 0).collect();
+    println!("{:?}",even_vec);
 }
